050c4f9a32d25bc17f8e67cee71208a10c8d8102
Fathan Yuda F <fathanupn17@gmail.com>
2023-04-06 13:26:41 +0700

    revisi slider

--- carousel.js
+++ carousel.js
@@ -1,35 +1,19 @@
-let slideIndex = 1;
-let timer;
+let slideIndex = 0;
+showSlides();
 
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  clearTimeout(timer);
-  showSlides(slideIndex += n);
+function showSlides() {
+let i;
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
+for (i = 0; i < slides.length; i++) {
+slides[i].style.display = "none";
 }
-
-function currentSlide(n) {
-  clearTimeout(timer);
-  showSlides(slideIndex = n);
+slideIndex++;
+if (slideIndex > slides.length) {slideIndex = 1}
+for (i = 0; i < dots.length; i++) {
+dots[i].className = dots[i].className.replace(" active", "");
 }
-
-function showSlides(n) {
-  const slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  timer = setTimeout(() => {
-    showSlides(slideIndex);
-  }, 3000);
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += " active";
+setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
